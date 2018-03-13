7e5504a95d77fc6159e58b2d61fc41df8f83fff4
shendy5381 <51167275@qq.com>
2018-03-13 09:51:28 +0800

    v3 changes

--- js/affix.js
+++ js/affix.js
@@ -4,6 +4,7 @@
  * ========================================================================
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * @Author play for change
  * ======================================================================== */
 
 
